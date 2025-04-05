020347d88f7aec3e12e8ef0a6619cd8b5a1a10f3
Mike Z. Luo <digi2nomad@yahoo.com>
2025-04-05 17:32:41 -0400

    aop, etc

--- src/main/java/org/digi2nomad/translationmemory/web/controller/TranslationmemoryRestControllerLogger.java
+++ src/main/java/org/digi2nomad/translationmemory/web/controller/TranslationmemoryRestControllerLogger.java
@@ -1,6 +1,7 @@
 package org.digi2nomad.translationmemory.web.controller;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -8,8 +9,15 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class TranslationmemoryRestControllerLogger {
+	
 	@Before("execution(* org.digi2nomad.translationmemory.web.controller.*.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         System.out.println("Before executing: " + joinPoint.getSignature().getName());
     }
+
+	@After("execution(* org.digi2nomad.translationmemory.web.controller.*.*(..))")
+	public void logAfter(JoinPoint joinPoint) {
+		System.out.println("After executing: " + joinPoint.getSignature().getName());
+	}
+	
 }
